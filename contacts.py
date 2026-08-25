9d276652a0bceff970a1fce900e177251e828a68
Rahul degra <74339831+rahuldegra1@users.noreply.github.com>
2026-08-25 13:14:14 +0530

    Add file persistence to contact book

--- contacts.py
+++ contacts.py
@@ -1,24 +1,43 @@
 contacts = []
+try:
+    with open("contact.txt", "r") as file:
+        lines = file.readlines()
+except:
+    lines = []
+
+for line in lines:
+    cleaned = line.strip()
+    parts = cleaned.split(",")
+    name = parts[0].strip()
+    phone = parts[1].strip()
+    email = parts[2].strip()
+    contact = {"name": name, "phone": phone, "email": email}
+    contacts.append(contact)
+
 while True:
     print("1. add new contact")
-    print("2. view contacts")
-    print("3. quit")
-    print("4. Delete contact")
-    choice = input("Enter choice..")
+    print("2. view contact")
+    print("3.quit")
+    print("4. delete contact")
+
+    choice = input("Enter choice: ")
 
     if choice == "1":
-       user1 = input("Name: ")
-       user2 = input("phone: ")
-       user3 = input("Email: ")
-       contact1 = {"name":user1, "phone": user2, "email": user3}
-       contacts.append(contact1)
+        name = input("Name: ")
+        phone = input("Phone: ")
+        email = input("Email: ")
+        contact = {"name": name, "phone": phone, "email": email}
+        contacts.append(contact)
+        with open("contact.txt", "a") as file:
+            file.write(f"{name}, {phone}, {email}\n")
+                       
     elif choice == "2":
         for contact in contacts:
             print(contact["name"], contact["phone"], contact["email"])
     elif choice == "3":
         break
     elif choice == "4":
-        name_to_delete = input("which name to Delete: ")
+        name_to_delete = input("which name to delete: ")
         for contact in contacts:
             if contact["name"] == name_to_delete:
                 contacts.remove(contact)
